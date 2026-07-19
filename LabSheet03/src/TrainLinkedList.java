297a81a10ed01cec3497d89ec94de85069526aa0
phimrisa <phimrisa@gmail.com>
2026-07-19 17:17:10 +0700

    Update TrainLinkedList.java

--- LabSheet03/src/TrainLinkedList.java
+++ LabSheet03/src/TrainLinkedList.java
@@ -3,58 +3,58 @@ public class TrainLinkedList {
 
 	public static void main(String[] args) {
 	
-		LinkedList greeline = new LinkedList();
+		LinkedList greenline = new LinkedList();
 		
 		// 1. Add Mo-chit to linked list.
-		greeline.insert("Mo-chit");
+		greenline.insert("Mo-chit");
 		
 		// 2. Add Ari to linked list.
-		greeline.insert("Ari");
+		greenline.insert("Ari");
 		
         // 3. Add Sanam-Pao to linked list.
-        greeline.insert("Sanam-Pao");
+        greenline.insert("Sanam-Pao");
         
         // 4. Show all stations.
-        System.out.println("All Station   : " + greeline.traversal());
+        System.out.println("All Station   : " + greenline.traversal());
         
         // 5. Add Rachakru between Ari and Sanam-Pao.
-        greeline.insert(2, "Rachakru");
+        greenline.insert(2,"Rachakru");
         
         // 6. Show all stations.
-        System.out.println("All Station   : " + greeline.traversal());
+        System.out.println("All Station   : " + greenline.traversal());
         
         // 7. Show the number of stations.
-        System.out.println("Total Station : " + greeline.length());
+        System.out.println("Total Station : " + greenline.length());
         
         // 8. Show the first station's name.
-        System.out.println("First Station : " + greeline.get(0));
+        System.out.println("First Station : " + greenline.get(0));
         
         // 9. Set Mo-chit to Mo-chit (Central Ladprao).
-        greeline.set(0, "Mo-chit (Central Ladprao)");
+        greenline.set(0, "Mo-chit (Central Ladprao)");
         
         // 10. Show all stations.
-        System.out.println("All Station   : " + greeline.traversal());
+        System.out.println("All Station   : " + greenline.traversal());
        
         // 11. Remove Rachakru station.
-        greeline.remove(2);
+        greenline.remove(2);
         
         // 12. Show all stations.
-        System.out.println("All Station   : " + greeline.traversal());
+        System.out.println("All Station   : " + greenline.traversal());
        
         // 13. Remove the last station.
-        greeline.removeLastElement();
+        greenline.removeLastElement();
        
         // 14. Show all stations.
-        System.out.println("All Station   : " + greeline.traversal());
+        System.out.println("All Station   : " + greenline.traversal());
         
         // 15. Clear all green line station.
-        greeline.clear();
+        greenline.clear();
         
         // 16. Show the number of stations.
-        System.out.println("Total Station : " + greeline.length());
+        System.out.println("Total Station : " + greenline.length());
        
         // 17. Show all stations.
-        System.out.println("All Station   : " + greeline.traversal());
+        System.out.println("All Station   : " + greenline.traversal());
 
 	}
 
