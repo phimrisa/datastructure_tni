8f62098efec83af958ae35f9dd73ae12bfaa386c
phimrisa <phimrisa@gmail.com>
2026-07-25 13:56:02 +0700

    2513110235 Submit LabSheet04

--- LabSheet04/src/TodoList.java
+++ LabSheet04/src/TodoList.java
@@ -7,22 +7,30 @@ public class TodoList {
 		todoList.insert("Finish Homework");
 		todoList.insert("Laundry");
 		todoList.insert("Group Meeting");
-		System.out.println(todoList.traversal());
+		System.out.println("To-do List = " + todoList.traversal());
 
 		todoList.insert(0, "Submit Report");
-		System.out.println(todoList.traversal());
+		System.out.println("To-do List = " + todoList.traversal());
 		
 		todoList.insert(1, "Buy Food");
-		System.out.println(todoList.traversal());
+		System.out.println("To-do List = " + todoList.traversal());
 		
 		todoList.insert("Go to Gym");
-		System.out.println(todoList.traversal());
+		System.out.println("To-do List = " + todoList.traversal());
 		
+		// No.4.1
 		todoList.remove();
-		System.out.println(todoList.traversal());
 		
-
-		System.out.println(todoList.backwardTraversal());
+		// No.4.2
+		todoList.remove(3);
+		
+		// No.4.3
+		todoList.remove(0);
+		System.out.println("To-do List = " + todoList.traversal());
+		
+		// No.5.2
+		System.out.print("\nBackward Traversal = " + todoList.backwardTraversal());
+		
 	}
 
 }
