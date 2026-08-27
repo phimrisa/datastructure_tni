04a51b2df6c756583b5c26eb7650df745c2c1e02
phimrisa <phimrisa@gmail.com>
2026-08-27 17:36:30 +0700

    2513110235 Submit LabSheet02

---
+++ LabSheet08/src/TreeApp.java
@@ -0,0 +1,33 @@
+
+public class TreeApp {
+
+	public static void main(String[] args) {
+
+		// Binary Tree 1
+		System.out.println("-------------------------------");
+		System.out.println("Binary Tree from createTree1()");
+		System.out.println("-------------------------------");
+		
+		BinaryTree tree = new BinaryTree();
+		tree.createTree1();
+		tree.printTree(tree.getRoot(), 0);
+		
+		// Binary Tree 2
+		System.out.println("-------------------------------");
+		System.out.println("Binary Tree from createTree2()");
+		System.out.println("-------------------------------");
+		
+		tree.createTree2();
+		tree.printTree(tree.getRoot(), 0);
+		
+		// Binary Tree 3
+		System.out.println("-------------------------------");
+		System.out.println("Binary Tree from createTree3()");
+		System.out.println("-------------------------------");
+		
+		tree.createTree3();
+		tree.printTree(tree.getRoot(), 0);
+	
+	}
+
+}
